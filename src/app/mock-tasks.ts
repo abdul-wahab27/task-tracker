40a54aec86cedcf9e5187e3a159f14ae437f1d6d
Abdul Wahab <wahab.asghar27@gmail.com>
2022-03-09 17:35:50 -0500

    Task Tracker

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "May 5th at 2:30",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at school",
+    day: "May 6h at 1:30",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Shopping",
+    day: "May 7th at 12:30",
+    reminder: false,
+  },
+];
